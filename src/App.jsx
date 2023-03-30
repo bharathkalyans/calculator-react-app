aa861fd8d19426474a2394fe560c27efcc6cde31
Bharath Kalyan S <sbharath35@gmail.com>
2023-03-30 15:23:22 +0530

    Done with front end part

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import {useReducer} from "react";
+
+function reducer() {
+
+}
+
+function App() {
+
+    const [] = useReducer(reducer, {});
+
+    return (
+        <div className="App">
+            <div className="calculator-grid">
+                <div className="output">
+                    <div className="previous-operand"></div>
+                    <div className="current-operand"></div>
+                </div>
+                <button className="span-two">AC</button>
+                <button>DEL</button>
+                <button>%</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>*</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>+</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>-</button>
+                <button>.</button>
+                <button>0</button>
+                <button className="span-two">=</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default App
